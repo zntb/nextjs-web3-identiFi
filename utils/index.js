2401eadbae8cb340a4dd7396778c592b66925cc1
zntb <zenetibi@yahoo.com>
2024-12-15 11:01:59 +0200

    Add Utils and lib directory (Writing queries to interact with the smart contract functions in the frontend)

---
+++ utils/index.js
@@ -0,0 +1,17 @@
+import { ethers } from 'ethers';
+import identiFi from './identiFi.json';
+
+export const contract = async () => {
+  const provider = new ethers.providers.Web3Provider(window.ethereum);
+  const { ethereum } = window;
+  if (ethereum) {
+    const signer = provider.getSigner();
+    const contractReader = new ethers.Contract(
+      '0xaBc01BECcFe4f80cE26BFBA73c258b2EDb866fE8',
+      identiFi.abi,
+      signer,
+    );
+
+    return contractReader;
+  }
+};
