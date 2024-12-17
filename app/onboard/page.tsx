b9b50e0630a3b51abad6fd025f9472697e9313bf
zntb <zenetibi@yahoo.com>
2024-12-17 16:10:38 +0200

    Add Onboard page, setup Pinata

---
+++ app/onboard/page.tsx
@@ -0,0 +1,13 @@
+import Navbar from '@/components/navbar/navbar';
+import ContactForm from './contact-form';
+
+const OnBoard = () => {
+  return (
+    <div>
+      <Navbar />
+      <ContactForm />
+    </div>
+  );
+};
+
+export default OnBoard;
