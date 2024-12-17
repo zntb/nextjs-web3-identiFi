946cf79986a7c6babd50ad05667e0dc8b786661f
zntb <zntbdev@proton.me>
2024-12-17 16:21:23 +0200

    Add Profile, Dashboard

---
+++ app/dashboard/components/profile.tsx
@@ -0,0 +1,11 @@
+import Section from './section';
+
+const VerifyIdentity = () => {
+  return (
+    <div className='w-full'>
+      <Section />
+    </div>
+  );
+};
+
+export default VerifyIdentity;
