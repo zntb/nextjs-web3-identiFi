59bdbafa41612b9cd8f2bf1840e701ec561ee6b2
zntb <zntbdev@proton.me>
2024-12-17 19:10:33 +0200

    Update import

--- utils/index.js
+++ utils/index.js
@@ -1,5 +1,5 @@
 import { ethers } from 'ethers';
-import identiFi from './identiFi.json';
+import identiFi from '@/utils/identiFi.json';
 
 export const contract = async () => {
   const provider = new ethers.providers.Web3Provider(window.ethereum);
